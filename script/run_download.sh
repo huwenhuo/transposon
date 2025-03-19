40d17496b773a94c83697aec7544ce5d993b1b68
huwenhuo <huwenhuo@gmail.com>
2025-03-18 21:18:19 -0400

    update

---
+++ script/run_download.sh
@@ -0,0 +1,16 @@
+singularity run /home/whu78/images/sratoolkit_latest.sif prefetch -O fastq SRR1910731
+singularity run /home/whu78/images/sratoolkit_latest.sif prefetch -O fastq SRR1910732
+singularity run /home/whu78/images/sratoolkit_latest.sif prefetch -O fastq SRR1910733
+singularity run /home/whu78/images/sratoolkit_latest.sif prefetch -O fastq SRR1910734
+singularity run /home/whu78/images/sratoolkit_latest.sif prefetch -O fastq SRR1910735
+singularity run /home/whu78/images/sratoolkit_latest.sif prefetch -O fastq SRR1910736
+singularity run /home/whu78/images/sratoolkit_latest.sif prefetch -O fastq SRR1910737
+singularity run /home/whu78/images/sratoolkit_latest.sif prefetch -O fastq SRR1910738
+singularity run /home/whu78/images/sratoolkit_latest.sif prefetch fasterq-dump --split-filesfastq/SRR1910731/SRR1910731.sra
+singularity run /home/whu78/images/sratoolkit_latest.sif prefetch fasterq-dump --split-filesfastq/SRR1910732/SRR1910732.sra
+singularity run /home/whu78/images/sratoolkit_latest.sif prefetch fasterq-dump --split-filesfastq/SRR1910733/SRR1910733.sra
+singularity run /home/whu78/images/sratoolkit_latest.sif prefetch fasterq-dump --split-filesfastq/SRR1910734/SRR1910734.sra
+singularity run /home/whu78/images/sratoolkit_latest.sif prefetch fasterq-dump --split-filesfastq/SRR1910735/SRR1910735.sra
+singularity run /home/whu78/images/sratoolkit_latest.sif prefetch fasterq-dump --split-filesfastq/SRR1910736/SRR1910736.sra
+singularity run /home/whu78/images/sratoolkit_latest.sif prefetch fasterq-dump --split-filesfastq/SRR1910737/SRR1910737.sra
+singularity run /home/whu78/images/sratoolkit_latest.sif prefetch fasterq-dump --split-filesfastq/SRR1910738/SRR1910738.sra
